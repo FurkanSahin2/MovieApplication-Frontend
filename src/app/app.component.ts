6908c994149d392d9a631006d8182996f2f3026a
Furkan Sahin <aliince.000000@gmail.com>
2024-07-30 02:07:04 +0300

    Updated - 30.07.2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, CommonModule]
 })
 export class AppComponent {
-  title = 'MovieApplication';
+  title:string = 'MovieApplication';
+  user:string = "Furkan Şahin";
+  movie = {movieId: 1, movieName: "The Family", director: "Mr. Nobody"};
+  movie2 = {movieId: 2, movieName: "The Family1", director: "Mr. Nobody"};
+  movie3 = {movieId: 2, movieName: "The Family3", director: "Mr. Nobody"};
+  movie4 = {movieId: 3, movieName: "The Family5", director: "Mr. Nobody"};
+  movie5 = {movieId: 1, movieName: "The Family8", director: "Mr. Nobody"};
+  movie6 = {movieId: 1, movieName: "The Family8", director: "Mr. Nobody"};
+
+  movies = [this.movie, this.movie2, this.movie3, this.movie4, this.movie5, this.movie6];
+
 }
