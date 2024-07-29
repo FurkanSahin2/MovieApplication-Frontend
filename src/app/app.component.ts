7ee21fb1bf10a050c78900c58f6f0cf31fff36cb
Furkan Sahin <aliince.000000@gmail.com>
2024-07-30 02:08:29 +0300

    Updated - 30.07.2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MovieComponent } from "./components/movie/movie.component";
+import { NaviComponent } from "./components/navi/navi.component";
+import { CategoryComponent } from "./components/category/category.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, CommonModule]
+  imports: [RouterOutlet, CommonModule, MovieComponent, NaviComponent, CategoryComponent]
 })
 export class AppComponent {
   title:string = 'MovieApplication';
   user:string = "Furkan Şahin";
-  movie = {movieId: 1, movieName: "The Family", director: "Mr. Nobody"};
-  movie2 = {movieId: 2, movieName: "The Family1", director: "Mr. Nobody"};
-  movie3 = {movieId: 2, movieName: "The Family3", director: "Mr. Nobody"};
-  movie4 = {movieId: 3, movieName: "The Family5", director: "Mr. Nobody"};
-  movie5 = {movieId: 1, movieName: "The Family8", director: "Mr. Nobody"};
-  movie6 = {movieId: 1, movieName: "The Family8", director: "Mr. Nobody"};
-
-  movies = [this.movie, this.movie2, this.movie3, this.movie4, this.movie5, this.movie6];
-
 }
--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+
 
 @Component({
   selector: 'app-movie',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './movie.component.html',
-  styleUrl: './movie.component.css'
+  styleUrls: ['./movie.component.css']
 })
-export class MovieComponent {
 
+
+
+export class MovieComponent {
+  movie = { movieId: 1, movieName: 'The Family', director: 'Mr. Nobody' };
+  movie2 = { movieId: 2, movieName: 'The Family1', director: 'Mr. Nobody' };
+  movie3 = { movieId: 2, movieName: 'The Family3', director: 'Mr. Nobody' };
+  movie4 = { movieId: 3, movieName: 'The Family5', director: 'Mr. Nobody' };
+  movie5 = { movieId: 1, movieName: 'The Family8', director: 'Mr. Nobody' };
+  movie6 = { movieId: 1, movieName: 'The Family8', director: 'Mr. Nobody' };
+
+  movies = [
+    this.movie,
+    this.movie2,
+    this.movie3,
+    this.movie4,
+    this.movie5,
+    this.movie6,
+  ];
 }
